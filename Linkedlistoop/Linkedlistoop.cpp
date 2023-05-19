a8b080b2894265c544217f0d87ab208cb1ff53d0
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-19 17:56:46 +0700

    Mengunjungi dan membaca data dalam list

--- Linkedlistoop/Linkedlistoop.cpp
+++ Linkedlistoop/Linkedlistoop.cpp
@@ -105,3 +105,20 @@ bool List::search(int nim, Node** previous, Node** current)
 	return (*current != NULL);
 }
 
+void List::traverse()/*Mengunjungi dan membaca data dalam list*/
+{
+
+	if (listEmpty())
+		cout << "\nList Kosong\n";
+	else
+	{
+		cout << endl << "Data didalam list adalah :" << endl;
+		Node* currentNode;
+		for (currentNode = START; currentNode != NULL; currentNode = currentNode->next)
+		{
+			cout << currentNode->noMhs << "   " << currentNode->nama << "\n";
+		}
+		cout << endl;
+	}
+}
+
