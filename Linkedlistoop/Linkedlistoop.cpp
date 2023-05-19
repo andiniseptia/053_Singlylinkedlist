ca04b7cd697236605d22b03b58431047c79a278c
andiniseptia <andiniseptiandri124@gmail.com>
2023-05-19 17:47:24 +0700

    Menghapus node dari dalam list

--- Linkedlistoop/Linkedlistoop.cpp
+++ Linkedlistoop/Linkedlistoop.cpp
@@ -79,3 +79,15 @@ bool List::listEmpty()
 	else
 		return false;
 }
+bool List::delNode(int nim)/*Menghapus node dari dalam list*/
+{
+	Node* current, * previous;
+	if (search(nim, &previous, &current) == false)
+		return false;
+	previous->next = current->next;
+	if (current == START)
+		START = START->next;
+
+	delete current;
+	return true;
+}
